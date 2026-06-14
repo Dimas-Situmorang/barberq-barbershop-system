4f4c64031d6453d604c4696ddd3592bfc5cbe4b6
Dimas-Situmorang <dimarebel1@gmail.com>
2026-06-15 01:43:01 +0700

    connect database in vercel api route

--- src/pages/api/[...path].ts
+++ src/pages/api/[...path].ts
@@ -1,7 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createApp } from "../../../server/app";
+import { connectDB } from "../../../server/config/db";
 
 const app = createApp();
+let connectionPromise: Promise<void> | null = null;
 
 export const config = {
   api: {
@@ -10,6 +12,9 @@ export const config = {
   }
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  connectionPromise ??= connectDB();
+  await connectionPromise;
+
   return app(req, res);
 }
